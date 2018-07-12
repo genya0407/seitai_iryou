d49c5cc2d5c78319c1b68ca432f64f7bdd07d5dd
Yusuke Sangenya <longinus.eva@gmail.com>
2018-07-12 18:14:37 +0900

    Modified.

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,7 @@
 extern crate nalgebra as na;
+extern crate rand;
 use std::fmt;
-
-// 逆投影をした後に反復的に改善を繰り返すことで、より鮮明な像を得ることができるのではないか？
-// 改善：列、行の合計とスキャン値を比較し、合計値がスキャン値に近づくように修正を加える。
+use rand::prelude::*;
 
 fn original_image() -> Image {
     let mut original_image = na::DMatrix::<f32>::zeros(5, 5);
@@ -16,6 +15,25 @@ fn original_image() -> Image {
     return Image::new(original_image);
 }
 
+fn random_image(nrows: usize, ncols: usize) -> Image {
+    let mut rng = thread_rng();
+    let data = na::DMatrix::<f32>::from_fn(nrows, ncols, |_, _| rng.gen_range(0, 100) as f32);
+    Image::new(data)
+}
+
+fn sparse_random_image(nrows: usize, ncols: usize) -> Image {
+    let mut random_image = random_image(nrows, ncols);
+    random_image.data.apply(|val| {
+        if val > 70. {
+            val
+        } else {
+            0.
+        }
+    });
+    random_image
+}
+
+#[derive(Clone)]
 struct Image {
     pub data: na::DMatrix<f32>
 }
@@ -32,7 +50,7 @@ impl Image {
     }
 
     fn rows_projection(&self) -> na::DVector<f32> {
-        let mut v = na::DVector::<f32>::zeros(self.data.nrows());
+        let mut v = na::DVector::<f32>::zeros(self.data.ncols());
         v.fill(1.);
         self.data.clone() * v
     }
@@ -41,6 +59,12 @@ impl Image {
         let mut v = na::DVector::<f32>::zeros(self.data.nrows());
         v.fill(1.);
         self.data.clone().transpose() * v
+    }
+
+    fn round(&self) -> Self {
+        let mut img = self.clone();
+        img.data.apply(|val| val.round());
+        return img;
     }
 }
 
@@ -90,14 +114,33 @@ impl ImageInferrer {
 }
 
 fn main() {
+    println!("===== Given Image =====\n");
     let orig_image = original_image();
+    println!("Original Image:");
+    println!("{}", orig_image);
     let rows_proj = orig_image.rows_projection();
     let cols_proj = orig_image.cols_projection();
-    let mut inferred = ImageInferrer::infer_by_back_projection(rows_proj, cols_proj);
-    for _ in 0..1000 {
-        inferred.adjust();
+    let mut inferer = ImageInferrer::infer_by_back_projection(rows_proj, cols_proj);
+    println!("Back Projection:");
+    println!("{}", inferer.image.round());
+    for _ in 0..100 {
+        inferer.adjust();
     }
-    println!("{}", inferred.image);
-    inferred.image.data.apply(|val| val.round());
-    println!("{}", inferred.image);
+    println!("Improved Back Projection:");
+    println!("{}", inferer.image.round());
+
+    println!("===== Random Image =====\n");
+    let orig_image = sparse_random_image(5, 5);
+    println!("Original Image:");
+    println!("{}", orig_image);
+    let rows_proj = orig_image.rows_projection();
+    let cols_proj = orig_image.cols_projection();
+    let mut inferer = ImageInferrer::infer_by_back_projection(rows_proj, cols_proj);
+    println!("Back Projection:");
+    println!("{}", inferer.image.round());
+    for _ in 0..100 {
+        inferer.adjust();
+    }
+    println!("Improved Back Projection:");
+    println!("{}", inferer.image.round());
 }
